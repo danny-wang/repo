8b74cd7371ef62c0a0396f800226592fbb35f467
ubuntu <ubuntu@localhost.localdomain>
2019-12-16 17:04:34 +0800

    update

--- httpgzip/httpgzip.go
+++ httpgzip/httpgzip.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	"compress/gzip"
 	"fmt"
-	"golang.org/x/net/lex/httplex"
+	"golang.org/x/net/http/httpguts"
 	"io"
 	"mime"
 	"net/http"
@@ -33,7 +33,7 @@ type NotWorthGzipCompressing interface {
 // it applies gzip compression on the fly, if it's found to be beneficial.
 func ServeContent(w http.ResponseWriter, req *http.Request, name string, modTime time.Time, content io.ReadSeeker) {
 	// If client doesn't accept gzip encoding, serve without compression.
-	if !httplex.HeaderValuesContainsToken(req.Header["Accept-Encoding"], "gzip") {
+	if !httpguts.HeaderValuesContainsToken(req.Header["Accept-Encoding"], "gzip") {
 		http.ServeContent(w, req, name, modTime, content)
 		return
 	}
